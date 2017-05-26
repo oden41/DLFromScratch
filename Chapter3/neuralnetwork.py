37581eabf33b1a829e2163118e996f76fb8de1f8
oden <shumpmita@ic.dis.titech.ac.jp>
2017-05-26 23:32:13 +0900

    * 3.6.3まで

--- Chapter3/neuralnetwork.py
+++ Chapter3/neuralnetwork.py
@@ -1,4 +1,9 @@
+import sys, os
+sys.path.append(os.pardir)
+import pickle
+from dataset.mnist import load_mnist
 import numpy as np
+from PIL import Image
 import matplotlib.pyplot as plt
 
 
@@ -15,18 +20,28 @@ def relu(x):
     return np.maximum(0.0, x)
 
 
+def softmax(a):
+    c = np.max(a)
+    exp_a = np.exp(a - c)
+    sum_a = np.sum(exp_a)
+    return exp_a / sum_a
+
+
 def init_network():
-    network = {}
-    network['W1'] = np.array([[0.1, 0.3, 0.5], [0.2, 0.4, 0.6]])
-    network['b1'] = np.array([0.1, 0.2, 0.3])
-    network['W2'] = np.array([[0.1, 0.4], [0.2, 0.5], [0.3, 0.6]])
-    network['b2'] = np.array([0.1, 0.2])
-    network['W3'] = np.array([[0.1, 0.3], [0.2, 0.4]])
-    network['b3'] = np.array([0.1, 0.2])
+    with open("sample_weight.pkl", 'rb') as f:
+        network = pickle.load(f)
     return network
 
 
-def forward(network, x):
+def img_show(img):
+    pil_img = Image.fromarray(np.uint8(img))
+    pil_img.show()
+
+def get_data():
+    (x_train, t_train), (x_test, t_test) = load_mnist(flatten=True, normalize=True)
+    return x_test,t_test
+
+def predict(network,x):
     W1, W2, W3 = network['W1'], network['W2'], network['W3']
     b1, b2, b3 = network['b1'], network['b2'], network['b3']
 
@@ -35,12 +50,18 @@ def forward(network, x):
     a2 = np.dot(z1, W2) + b2
     z2 = sigmoid(a2)
     a3 = np.dot(z2, W3) + b3
-    y = a3
-
+    y = softmax(a3)
     return y
 
-
+x,t = get_data()
 network = init_network()
-x = np.array([1.0, 0.5])
-y = forward(network, x)
-print(y)
+
+acc = 0
+for i in range(len(x)):
+    y = predict(network, x[i])
+    p = np.argmax(y)
+    if p == t[i]:
+        acc += 1
+
+print('Accuracy:' + str(acc / len(x)))
+
